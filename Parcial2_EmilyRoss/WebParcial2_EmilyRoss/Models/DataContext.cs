bc564c0ce23c19f79373bc0bd0729809c4797049
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-24 11:19:52 -0400

    Creando el DataContext para el mvc

---
+++ Parcial2_EmilyRoss/WebParcial2_EmilyRoss/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebParcial2_EmilyRoss.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}

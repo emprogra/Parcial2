58588289df7048600181229b6fe05d5eb59718b3
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-24 10:20:27 -0400

    Creando FriendsController

---
+++ Parcial2_EmilyRoss/Parcial2_EmilyRoss/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2_EmilyRoss.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Parcial2_EmilyRoss.Models.Friend> Friends { get; set; }
+    }
+}
